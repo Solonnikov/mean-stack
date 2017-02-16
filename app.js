42ab59dfd9d10a59832c94f40fed47816dc577cc
Yaroslav Solonnikov <yaroslav.solonnikov@gmail.com>
2017-02-16 22:51:25 +0200

    added authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const config = require('./config/database');
 
 // Connect to Database
+mongoose.Promise = global.Promise;
 mongoose.connect(config.database);
 
 // On connection
@@ -28,11 +29,17 @@ const port = 3000;
 // CORS Middleware
 app.use(cors());
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Body Parser Middleware
 app.use(bodyParser.json());
 
-// Set Static Folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 app.use('/users', users);
 
