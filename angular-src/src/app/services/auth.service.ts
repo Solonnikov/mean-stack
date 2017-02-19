c30281256d40410264926554ff908dddec74ada4
Yaroslav Solonnikov <yaroslav.solonnikov@gmail.com>
2017-02-19 11:53:41 +0200

    added protected requests and finished the app

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import {tokenNotExpired} from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -13,6 +14,15 @@ export class AuthService {
     let  headers = new Headers();
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
+    .map(res => res.json());
+  }
+
+  getProfile() {
+    let  headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', {headers: headers})
     .map(res => res.json());
   }
 
@@ -29,6 +39,16 @@ export class AuthService {
     this.authToken - token;
     this.user - user;
   }
+
+  loadToken() {
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
+  loggedIn() {
+    return tokenNotExpired();
+  }
+
   logout() {
     this.authToken = null;
     this.user = null;
